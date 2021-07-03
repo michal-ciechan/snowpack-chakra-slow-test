3501c251b34f6dda5ffc966a7bf9449e0d57bb17
Michal Ciechan <lndcobra@hotmail.com>
2021-07-03 23:11:36 +0100

    Installing and using chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,31 @@
-import React, { useState, useEffect } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  Center,
+  ChakraProvider,
+  CSSReset,
+  extendTheme,
+} from '@chakra-ui/react';
 
 interface AppProps {}
 
+const theme = extendTheme({
+  components: {
+    Button: {
+      baseStyle: {
+        _focus: {
+          boxShadow: 'none',
+        },
+      },
+    },
+  },
+});
+
 function App({}: AppProps) {
-  // Create the count state.
-  const [count, setCount] = useState(0);
-  // Create the counter (+1 every second).
-  useEffect(() => {
-    const timer = setTimeout(() => setCount(count + 1), 1000);
-    return () => clearTimeout(timer);
-  }, [count, setCount]);
-  // Return the App component.
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <p>
-          Page has been open for <code>{count}</code> seconds.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+    <ChakraProvider theme={theme}>
+      <CSSReset />
+      <Center height="100vh"></Center>
+    </ChakraProvider>
   );
 }
 
